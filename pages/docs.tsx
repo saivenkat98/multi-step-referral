880ae41aaa17aa95042e584d659f60f162fb194c
sai venkat <saivenkatrapol@gmail.com>
2025-05-04 19:20:50 -0400

    made changes to facilitate successful docker build

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,5 +1,10 @@
-import SwaggerUI from 'swagger-ui-react';
+'use client';
+
+import React from 'react';
+import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
+
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
 
 export default function ApiDocs() {
   return (
